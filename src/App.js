6dff807f63c3c0aa93df4d793edfd4379210e656
Christian Ramirez <christian.r0602@gmail.com>
2022-06-17 12:00:05 -0500

    css for recipe component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import RecipeList from './components/RecipeList';
+import './css/App.css'
 
 function App() {
   const sampleRecipes = [
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default function Recipe(props) {
   const {name, time, servings, instructions, ingredients}= props
   return (
-    <div>
-      <div>
-        <h2>{name}</h2>
+    <div className='recipe'>
+      <div className='header'>
+        <h2 className='title-font'>{name}</h2>
         <button>Edit</button>
         <button>Delete</button>
       </div>
-      <div>
+      <div className='body body-font'>
         <p>Cook Time: {time}</p>
         <p>Servings: {servings}</p>
       </div>
@@ -19,12 +19,17 @@ export default function Recipe(props) {
       <li>{instructions.two}</li>
       <li>{instructions.three}</li>
       </ol>
-      <div>
-        <p>Ingredients:</p>
+      <h2>Instructions</h2>
+      <div className='footer'>
+        <div className='footer-items'>
         <p>{ingredients.name}</p>
         <span>{ingredients.qty}</span>
+        </div>
+        <div className='footer-items'>
         <p>{ingredients.seasoning}</p>
         <span>{ingredients.amount}</span>
+        </div>
+      
       </div>
     </div>
    
