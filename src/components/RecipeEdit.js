045f65c07703b24f48ce8cf245664e88b1251b2a
Christian Ramirez <christian.r0602@gmail.com>
2022-06-20 19:43:38 -0500

    styling the form

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -4,22 +4,28 @@ import RecipeIngredientsEdit from './RecipeIngredientsEdit'
 export default function RecipeEdit() {
   return (
     <div className='edit-recipe'>
-      <div>
+      <div className='btn-wrapper'>
+        <button className='exit-btn'>&times;</button>
+      </div>
+      <form className='recipe-form'>
+      <div className='form-input'>
         <label>Name: </label>
         <input type='text'></input>
       </div>
-      <div>
+      <div className='form-input'>
         <label>Cook Time: </label>
         <input type='text'></input>
       </div>
-      <div>
+      <div className='form-input'>
         <label>Servings: </label>
         <input type='text'></input>
       </div>
-      <div>
+      <div className='form-input'>
         <label>Instructions: </label>
-        <input type='text'></input>
+        <textarea></textarea>
       </div>
+      </form>
+
       <br/>
       <label>Ingredients</label>
       <div>
